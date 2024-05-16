96a6fd7930c13900b28c7f3ac4c2ce5711c25b37
Chad W Pry <chad.pry@gmail.com>
2024-05-16 13:23:57 -0500

    fix naming on referenced tmux plugin

--- nvim/lua/configs/null-ls.lua
+++ nvim/lua/configs/null-ls.lua
@@ -4,7 +4,7 @@ local augroup = vim.api.nvim_create_augroup("LspFormating", {})
 local opts = {
   sources = {
     null_ls.builtins.formatting.gofumpt,
-    null_ls.builtins.formatting.goimports_revisor,
+    null_ls.builtins.formatting.goimports_reviser,
     null_ls.builtins.formatting.golines,
   },
   on_attach = function(client, bufnr)
