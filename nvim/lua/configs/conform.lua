79d5107f3be069933f3c4ab01a9e306d3107b10c
Chad W Pry <chad.pry@gmail.com>
2024-05-20 17:54:33 -0500

    update nvim settings
    - move nvim-dap configuration into configs folder
    - change lua style for parenthesis
    - add linting and formatting for multiple languages

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -1,15 +1,32 @@
-local options = {
+local conform = require("conform")
+
+conform.setup({
   formatters_by_ft = {
+    css = { "prettier" },
+    go = { "gofumpt" },
+    html = { "prettier" },
+    graphql = { "prettier" },
+    javascript = { "prettier" },
+    javascriptreact = { "prettier" },
+    json = { "prettier" },
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    markdown = { "stylua" },
+    python = { "isort", "black" },
+    typescript = { "prettier" },
+    typescriptreact = { "prettier" },
+    yaml = { "prettier" },
   },
+  format_on_save = {
+    lsp_fallback = true,
+    async = false,
+    timeout_ms = 500,
+  },
+})
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
-}
-
-require("conform").setup(options)
+vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+  conform.format({
+    lsp_fallback = true,
+    async = false,
+    timeout_ms = 500,
+  })
+end, { desc = "Format file or range (in visual mode)" })
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,9 +1,9 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
-      require "configs.conform"
+      require("configs.conform")
     end,
   },
 
@@ -12,22 +12,46 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       require("nvchad.configs.lspconfig").defaults()
-      require "configs.lspconfig"
+      require("configs.lspconfig")
     end,
   },
   {
-  	"williamboman/mason.nvim",
-  	opts = {
-  		ensure_installed = {
-  			"lua-language-server",
-        "stylua", "html-lsp", "css-lsp" ,
-        "prettier", "eslint_d",
+    "williamboman/mason.nvim",
+    config = function()
+      local mason = require("mason")
+
+      mason.setup({})
+    end,
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "eslint_d",
         -- "rubocop", # install ruby development environment first
-        "gopls", "gofumpt", "goimports-reviser", "golines", "delve",
-        "black", "isort",
-  		},
-  	},
+        "gopls",
+        "gofumpt",
+        "goimports-reviser",
+        "golines",
+        "delve",
+        "black",
+        "isort",
+        "pylint",
+      },
+    },
   },
+  -- {
+  --   "mfussenegger/nvim-lint",
+  --   event = {
+  --     "BufWritePre",
+  --     "BufNewFile",
+  --   },
+  --   config = function()
+  --     require("configs.nvim-lint")
+  --   end,
+  -- },
   {
     "mfussenegger/nvim-dap",
     dependencies = {
@@ -36,76 +60,25 @@ return {
       "nvim-neotest/nvim-nio",
     },
     config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-
-      require("dapui").setup()
-      require("dap-go").setup()
-
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
-
-      vim.keymap.set("n", "<Leader>ds", function()
-        dap.continue()
-        dapui.toggle({})
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false)
-      end)
-      vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
-      vim.keymap.set("n", "<Leader>ds", function()
-        dap.continue()
-        dapui.toggle({})
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false)
-      end)
-      vim.keymap.set("n", "<Leader>de", function()
-        dap.terminate()
-        dapui.toggle({})
-      end)
-      vim.keymap.set("n", "<Leader>dsd", dap.step_into, {})
-      vim.keymap.set("n", "<Leader>dso", dap.step_over, {})
-      vim.keymap.set("n", "<Leader>dsu", dap.step_out, {})
-      vim.keymap.set("n", "<Leader>dr", function()
-        dap.repl.toggle()
-      end)
-      vim.keymap.set("n", "<Leader>df", function()
-        local widgets = require("dap.ui.widgets")
-        widgets.centered_float(widgets.frames)
-      end)
-
-      vim.fn.sign_define("DapBreakpoint", { text="🛑", texthl="", numhl="" })
+      require("configs.nvim-dap")
     end,
   },
-  -- {
-  --   -- "dreamsofcode-io/nvim-dap-go",
-  --   "leoluz/nvim-dap-go",
-  --   ft = "go",
-  --   dependencies = "mfussenegger/nvim-dap",
-  --   -- config = function(_, opts)
-  --   --   require("dap-go").setup(opts)
-  --   --end
-  -- },
   {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css"
-  		},
-  	},
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+      },
+    },
   },
   {
     "nvimtools/none-ls.nvim",
     opts = function()
-      return require "configs/none-ls"
-    end
+      return require("configs/none-ls")
+    end,
   },
 }
