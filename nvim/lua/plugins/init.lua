24d78087d94a93f892ae5844006c031a3bff7590
Chad W Pry <chad.pry@gmail.com>
2024-07-09 21:09:39 -0500

    correct loading configuration

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,42 +1,86 @@
 return {
   {
+    "numToStr/Comment.nvim",
+    lazy = false,
+    opts = function()
+      require("configs.comment")
+    end,
+  },
+  {
     "stevearc/conform.nvim",
+    lazy = true,
     event = { "BufReadPre", "BufNewFile" },
-    config = function()
+    opts = function()
       require("configs.conform")
     end,
   },
   {
+    "williamboman/mason.nvim",
+    opts = function()
+      return {
+        ensure_installed = {
+          "lua-language-server",
+          "stylua",
+          "html-lsp",
+          "css-lsp",
+          "prettier",
+          "eslint_d",
+          -- "rubocop", # install ruby development environment first
+          "gopls",
+          "gofumpt",
+          "goimports-reviser",
+          "golines",
+          "delve",
+          "black",
+          "isort",
+          "pylint",
+        },
+      }
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function()
+      require("configs.none-ls")
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
-    config = function()
+    opts = function()
       require("nvchad.configs.lspconfig").defaults()
       require("configs.lspconfig")
     end,
   },
   {
-    "williamboman/mason.nvim",
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "leoluz/nvim-dap-go",
+      "nvim-neotest/nvim-nio",
+    },
     config = function()
-      local mason = require("mason")
-
-      mason.setup({})
+      require("configs.nvim-dap")
     end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    event = {
+      "BufWritePre",
+      "BufNewFile",
+    },
+    config = function()
+      require("configs.nvim-lint")
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
-        "lua-language-server",
-        "stylua",
-        "html-lsp",
-        "css-lsp",
-        "prettier",
-        "eslint_d",
-        -- "rubocop", # install ruby development environment first
-        "gopls",
-        "gofumpt",
-        "goimports-reviser",
-        "golines",
-        "delve",
-        "black",
-        "isort",
-        "pylint",
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
       },
     },
   },
@@ -56,44 +100,5 @@ return {
       { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
       { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
     },
-  },
-  -- {
-  --   "mfussenegger/nvim-lint",
-  --   event = {
-  --     "BufWritePre",
-  --     "BufNewFile",
-  --   },
-  --   config = function()
-  --     require("configs.nvim-lint")
-  --   end,
-  -- },
-  {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      "rcarriga/nvim-dap-ui",
-      "leoluz/nvim-dap-go",
-      "nvim-neotest/nvim-nio",
-    },
-    config = function()
-      require("configs.nvim-dap")
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim",
-        "lua",
-        "vimdoc",
-        "html",
-        "css",
-      },
-    },
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    opts = function()
-      return require("configs/none-ls")
-    end,
   },
 }
