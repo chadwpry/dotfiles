e5764d7732eca9cd7448917e6d9cb412f942aafc
Chad W Pry <chad.pry@gmail.com>
2024-07-13 19:24:01 -0500

    change config to use existing reference

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -26,7 +26,7 @@ conform.setup({
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*",
   callback = function(args)
-    require("conform").format({ bufnr = args.buf })
+    conform.format({ bufnr = args.buf })
   end,
 })
 
