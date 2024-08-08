464480a5392e7558c53f42f8de9d7cc8aea09c70
Chad W Pry <chad.pry@gmail.com>
2024-08-08 14:24:12 -0500

    add neovim keymappings for sorting

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,6 +6,10 @@ local map = vim.keymap.set
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("v", "<Leader>sa", ":sort<enter>")
+map("v", "<Leader>sd", ":sort!<enter>")
+map("v", "<Leader>su", ":sort u<enter>")
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
 local dap = require("dap")
